d10c8929e9ae1416abb91f77d3e4adfb0bbac104
yingtianlee <yingtianlee@foxmail.com>
2017-05-10 15:17:52 +0800

    add new

---
+++ examples/inline/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('../..');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpackMajorVersion = require('webpack/package.json').version.split('.')[0];
+
+module.exports = {
+  entry: './example.js',
+  output: {
+    path: path.join(__dirname, 'dist/webpack-' + webpackMajorVersion),
+    publicPath: '',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+      { test: /\.jade$/, loader: 'jade' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      cache: false,
+      template: 'template.jade',
+      filename: 'index.html',
+      favicon: 'favicon.ico',
+      title: 'Jade demo'
+    }),
+    new ExtractTextPlugin('styles.css')
+  ]
+};
---
+++ examples/javascript-advanced/webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('../..');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpackMajorVersion = require('webpack/package.json').version.split('.')[0];
+module.exports = {
+  entry: './example.js',
+  output: {
+    path: path.join(__dirname, 'dist/webpack-' + webpackMajorVersion),
+    publicPath: '',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+      { test: /\.png$/, loader: 'file-loader' },
+      { test: /\.html$/, loader: 'html-loader' }
+    ]
+  },
+  devtool: 'eval',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'template.js'
+    }),
+    new ExtractTextPlugin('styles.css')
+  ]
+};
